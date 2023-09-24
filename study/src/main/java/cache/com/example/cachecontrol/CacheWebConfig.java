a1a8710ddceb2b9b68026c65cd156193d0c43c98
yeonkkk <tjddus0302@gmail.com>
2023-09-24 16:40:43 +0900

    refactor: 학습 테스트 0단계 - 휴리스틱 캐싱 제거하기

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,6 +1,12 @@
 package cache.com.example.cachecontrol;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +15,12 @@ public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                   ModelAndView modelAndView) throws Exception {
+                response.addHeader(HttpHeaders.CACHE_CONTROL, CacheControl.noCache().cachePrivate().getHeaderValue());
+            }
+        });
     }
 }
